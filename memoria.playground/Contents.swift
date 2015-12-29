f8e09ab91fe0d2e76fed6342c337ec7f5d4591da
Luis Manuel Espinosa <lmespinosa809@gmail.com>
2015-12-28 23:47:37 -0400

    final

--- memoria.playground/Contents.swift
+++ memoria.playground/Contents.swift
@@ -3,3 +3,20 @@
 import UIKit
 
 var str = "Hello, playground"
+
+for i in 0...100 {
+
+    if i % 10 == 5 || i % 10 == 0 {
+            print("# \(i) Bingo!!!")
+    }
+    if i % 10 == 2 || i % 10 == 0{
+          print("# \(i) par!!!")
+    }
+    if i % 10 == 3 || i % 10 == 1{
+        print("# \(i) impar!!!")
+    }
+    if i >= 30 && i <= 40 {
+        print("# \(i) Viva Swift!!!")
+    }
+
+}
